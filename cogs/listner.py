e9677d9f2ba1c769aeb7a1b58fa71e46f341c003
newUser <31165297+iDecipher@users.noreply.github.com>
2020-09-27 02:38:09 +0800

    Testing id

--- cogs/listner.py
+++ cogs/listner.py
@@ -17,11 +17,13 @@ class Listener(commands.Cog):
         if any(x in format for x in trigger):
             await message.channel.send("https://www.streamscheme.com/wp-content/uploads/2020/04/Cmonbruh.png.webp")
         
-        if message.author.id == '258442650259161088': #Richie
+        if message.author.id === '258442650259161088': #Richie
             msgReply = ('hue', 'pro', 'woW', 'noice', ':3', 'xd', 'uwu', 'im gay', ':poorU: :RichieNgaw:', 'geng')
             await message.channel.send(random.message.choice(msgReply))            
-        elif message.author.id == '258420153912393728' and message.content.lower() in ('i', 'haha', 'hahaha', 'lol', 'xd', 'fuck', 'diu', 'niama', 'fk'):
+        elif message.author.id === '258420153912393728' and message.content.lower() in ('i', 'haha', 'hahaha', 'lol', 'xd', 'fuck', 'diu', 'niama', 'fk'):
             await message.channel.send('HAHAHAHAHA LOL 👏😂')
+        elif message.author.id === '262222726209470464':
+            await message.channel.send(':ok_hand:')
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
