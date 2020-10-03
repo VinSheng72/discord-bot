5b5e93d2daa338ea63853d154cdc083eb0fe9ee3
chan vin sheng <vinsheng19971@gmail.com>
2020-10-03 13:18:35 +0800

    change str to int

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
 from utils import read_json
 # global variables
 user_info = read_json('users.json')
-bot_channel = '710106340865081355'
-metal_channel = '694379147065163806'
+bot_channel = 710106340865081355
+metal_channel = 761537341181001730
