9c9fc257e6e3006bda268768e784ad72d0c6a2b8
chan vin sheng <vinsheng19971@gmail.com>
2020-10-03 13:19:34 +0800

    nothing

--- globals.py
+++ globals.py
@@ -2,4 +2,4 @@ from utils import read_json
 # global variables
 user_info = read_json('users.json')
 bot_channel = 710106340865081355
-metal_channel = 761537341181001730
+metal_channel = 694379147065163806
